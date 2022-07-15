2e48c474dce9442633dd891e2dc6d38b23e0196b
Marco Tessitore <marcotexcode@gmail.com>
2022-07-15 15:23:06 +0200

    inizio creazione calendario con modale 'demo'

--- resources/js/components/Calendario.vue
+++ resources/js/components/Calendario.vue
@@ -1,18 +1,42 @@
+
 <template>
-    <div class="container">
-        <h2>Calendario</h2>
+    <div class="container glass mt-5 glass p-5">
+        <FullCalendar :options="calendarOptions" />
+        <div>
+            <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+            <b-modal id="modal-1" title="BootstrapVue">
+                <p class="my-4">Hello from modal!</p>
+            </b-modal>
+            </div>
     </div>
 </template>
 
 <script>
+import '@fullcalendar/core/vdom'
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
-    export default ({
-        name: "Calendario",
-
-    });
-
+export default {
+    name: 'Calendario',
+    components: {
+        FullCalendar
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [ dayGridPlugin, interactionPlugin ],
+                initialView: 'dayGridMonth',
+                dateClick: this.aggiungiEvento,
+            }
+        }
+    },
+    methods: {
+    aggiungiEvento: function(info) {
+      console.log(info);
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
