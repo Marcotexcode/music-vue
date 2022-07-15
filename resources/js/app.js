d29084b34ab3581f2b8aea3d5089a07a1015c7f5
Marco Tessitore <marcotexcode@gmail.com>
2022-07-15 15:22:11 +0200

    importazione nel file app.js bootstrap vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,15 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 
-// Importo vue da node_modules
 import Vue from 'vue'
-//window.Vue = require('vue').default;
-
-// Importo vueRouter da node_modules 
 import VueRouter from 'vue-router'
-
-//Prende il file index.js nella cartella routes che sarebbe il file dove inserisco tutte le rotte dei vari componenti
 import { routes } from './routes';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-//Vue.use ti impedisce automaticamente di utilizzare lo stesso plug-in più di una volta,
-// quindi chiamandolo più volte sullo stesso plug-in verrà installato il plug-in solo una volta.
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(VueRouter)
 
 
@@ -30,18 +21,8 @@ export const router = new VueRouter({
     routes
 });
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-// Registrazione componente 
+// Registrazione componente
 Vue.component('app-component', require('./views/App.vue').default);
 
 
@@ -51,7 +32,7 @@ Vue.component('app-component', require('./views/App.vue').default);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-// Creo una nuova instanza dell'aplicazione Vue 
+// Creo una nuova instanza dell'aplicazione Vue
  const app = new Vue({
     router
 }).$mount('#app')
