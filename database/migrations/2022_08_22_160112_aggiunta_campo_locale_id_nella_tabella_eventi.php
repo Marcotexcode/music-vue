3f95f90d32f69ec10fbc72f7ee126e5a78baa6d2
Marco Tessitore <marcotexcode@gmail.com>
2022-08-22 18:13:10 +0200

    Aggiunto campo locale_id e modifica aggiunta compenso nella tabella eventi

--- database/migrations/2022_08_22_160112_aggiunta_campo_locale_id_nella_tabella_eventi.php
+++ database/migrations/2022_08_22_160112_aggiunta_campo_locale_id_nella_tabella_eventi.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AggiuntaCampoCompensoAllaTabellaEventi extends Migration
+class AggiuntaCampoLocaleIdNellaTabellaEventi extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AggiuntaCampoCompensoAllaTabellaEventi extends Migration
     public function up()
     {
         Schema::table('eventi', function (Blueprint $table) {
-            $table->float('compenso', 8, 2)->after('ora');
+            $table->unsignedBigInteger('locale_id')->unsigned()->after('band_id');
+            $table->foreign('locale_id')->references('id')->on('locale')->onDelete('cascade');
         });
     }
 
@@ -26,7 +27,7 @@ class AggiuntaCampoCompensoAllaTabellaEventi extends Migration
     public function down()
     {
         Schema::table('eventi', function (Blueprint $table) {
-            $table->dropColumn('compenso')->after('ora');
+            $table->dropColumn('locale_id')->unsigned()->after('band_id');
         });
     }
 }
